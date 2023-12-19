ff6c9da25f15464629189e3cbd1718aa73553252
Prakash Paruchuri <prakash.paruchuri@microland.com>
2023-12-19 20:13:31 +0000

    AAD Admin

---
+++ modules/databases/mysql_flexible_server/authentication.tf
@@ -0,0 +1,10 @@
+resource "azurerm_mysql_flexible_server_active_directory_administrator" "mysql" {
+  for_each = try(var.settings.mysql_aad_admin, {})
+
+
+  server_id   = azurerm_mysql_flexible_server.mysql.id
+  identity_id = var.settings.identity.identity_ids
+  login       = "sqladmin"
+  object_id   = var.settings.admin_group_object_id
+  tenant_id   = data.azurerm_client_config.current.tenant_id
+}
