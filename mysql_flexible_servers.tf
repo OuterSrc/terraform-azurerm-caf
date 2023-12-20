db0e421ceb6614f263cba5f5f242346d30dcd118
Prakash Paruchuri <prakash.paruchuri@microland.com>
2023-12-20 13:56:39 +0000

    Update mysql_flexible_servers.tf

--- mysql_flexible_servers.tf
+++ mysql_flexible_servers.tf
@@ -12,9 +12,13 @@ module "mysql_flexible_server" {
   client_config   = local.client_config
   settings        = each.value
 
+  private_endpoints   = try(each.value.private_endpoints, {})
+  private_dns         = local.combined_objects_private_dns
+  vnets               = local.combined_objects_networking
+  resource_groups     = local.combined_objects_resource_groups
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   location            = can(local.global_settings.regions[each.value.region]) || can(each.value.region) ? try(local.global_settings.regions[each.value.region], each.value.region) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-
+  base_tags           = local.global_settings.inherit_tags
 
   remote_objects = {
     subnet_id           = can(each.value.vnet.subnet_key) ? local.combined_objects_networking[try(each.value.vnet.lz_key, local.client_config.landingzone_key)][each.value.vnet.key].subnets[each.value.vnet.subnet_key].id : null
