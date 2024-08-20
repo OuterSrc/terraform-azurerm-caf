7a39e9ae5d3a9ec40c852b55698c6a3ef3940ec7
ml-justinhewitt <72511644+ml-justinhewitt@users.noreply.github.com>
2024-08-20 15:19:47 +0100

    Update diagnostics.tf

--- modules/identity/active_directory_domain_service/diagnostics.tf
+++ modules/identity/active_directory_domain_service/diagnostics.tf
@@ -1,7 +1,7 @@
 module "diagnostics" {
   source = "../../diagnostics"
 
-  resource_id       = azurerm_active_directory_domain_service.aadds.id
+  resource_id       = azurerm_active_directory_domain_service.aadds.resource_id
   resource_location = azurerm_active_directory_domain_service.aadds.location
   diagnostics       = var.diagnostics
   profiles          = try(var.settings.diagnostic_profiles, {})
