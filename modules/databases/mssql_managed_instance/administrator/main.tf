02f15fecb15e6d8e835f130a6d9ba42b440672a6
ml-mithilkatipelly <mithil.katipelly@microland.com>
2024-11-21 13:38:33 +0000

    Update main.tf

--- modules/databases/mssql_managed_instance/administrator/main.tf
+++ modules/databases/mssql_managed_instance/administrator/main.tf
@@ -9,5 +9,5 @@ terraform {
 
 locals {
   object_id    = coalesce(var.group_id, try(data.azuread_user.upn[0].id, null))
-  display_name = var.group_id != null ? var.group_name : var.user_principal_name
+  display_name = var.group_id != null ? var.azuread_group_name : var.user_principal_name
 }
