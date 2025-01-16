b5f055ca5e0f8749ccb319f97cf268c67b50abe3
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2025-01-16 16:57:17 +0000

    debug

--- modules/networking/network_profile/module.tf
+++ modules/networking/network_profile/module.tf
@@ -1,18 +1,3 @@
-output "debug_ip_configuration" {
-  value = {
-    subnet_id = try(ip_configuration.value.subnet_id, null)
-    lz_key = try(ip_configuration.value.lz_key, null)
-    vnet_key = try(ip_configuration.value.vnet_key, null)
-    subnet_key = try(ip_configuration.value.subnet_key, null)
-    virtual_subnet_key = try(ip_configuration.value.virtual_subnet_key, null)
-    landingzone_key = try(var.client_config.landingzone_key, null)
-    networking_subnet_id = try(var.remote_objects.networking[ip_configuration.value.lz_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null)
-    networking_landingzone_subnet_id = try(var.remote_objects.networking[var.client_config.landingzone_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null)
-    virtual_networks_subnet_id = try(var.remote_objects.virtual_networks[ip_configuration.value.lz_key][ip_configuration.value.virtual_subnet_key].id, null)
-    virtual_networks_landingzone_subnet_id = try(var.remote_objects.virtual_networks[var.client_config.landingzone_key][ip_configuration.value.virtual_subnet_key].id, null)
-  }
-}
-
 resource "azurerm_network_profile" "this" {
   name                = var.settings.name
   location            = can(var.settings.location) ? var.settings.location : var.resource_group.location
@@ -34,6 +19,21 @@ resource "azurerm_network_profile" "this" {
           try(var.remote_objects.virtual_networks[ip_configuration.value.lz_key][ip_configuration.value.virtual_subnet_key].id, null),
           try(var.remote_objects.virtual_networks[var.client_config.landingzone_key][ip_configuration.value.virtual_subnet_key].id, null)
         )
+
+        output "debug_ip_configuration" {
+          value = {
+            subnet_id = try(ip_configuration.value.subnet_id, null)
+            lz_key = try(ip_configuration.value.lz_key, null)
+            vnet_key = try(ip_configuration.value.vnet_key, null)
+            subnet_key = try(ip_configuration.value.subnet_key, null)
+            virtual_subnet_key = try(ip_configuration.value.virtual_subnet_key, null)
+            landingzone_key = try(var.client_config.landingzone_key, null)
+            networking_subnet_id = try(var.remote_objects.networking[ip_configuration.value.lz_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null)
+            networking_landingzone_subnet_id = try(var.remote_objects.networking[var.client_config.landingzone_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null)
+            virtual_networks_subnet_id = try(var.remote_objects.virtual_networks[ip_configuration.value.lz_key][ip_configuration.value.virtual_subnet_key].id, null)
+            virtual_networks_landingzone_subnet_id = try(var.remote_objects.virtual_networks[var.client_config.landingzone_key][ip_configuration.value.virtual_subnet_key].id, null)
+          }
+        }
       }
     }
   }
