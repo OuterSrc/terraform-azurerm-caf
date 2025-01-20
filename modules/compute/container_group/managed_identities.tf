7df852a4ce1d39555d572abd206f71e1905de3f9
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2025-01-20 16:29:03 +0000

    tidy

--- modules/compute/container_group/managed_identities.tf
+++ modules/compute/container_group/managed_identities.tf
@@ -10,11 +10,11 @@ locals {
   ])
 
   managed_remote_identities = flatten([
-      for lz_key, value in try(var.settings.identity.remote, []) : [
-        for managed_identity_key in value.managed_identity_keys : [
-          var.combined_resources.managed_identities[lz_key][managed_identity_key].id
-        ]
+    for lz_key, value in try(var.settings.identity.remote, []) : [
+      for managed_identity_key in value.managed_identity_keys : [
+        var.combined_resources.managed_identities[lz_key][managed_identity_key].id
       ]
+    ]
   ])
 
   managed_identities = concat(local.managed_local_identities, local.managed_remote_identities, var.settings.identity.managed_identity_ids)
