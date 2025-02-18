f4291bc206289d1cf509a0e87a8f63cd0c13d709
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2025-02-18 16:50:21 +0000

    add vars file to sss

---
+++ modules/storage_sync/storage_sync_service/variables.tf
@@ -0,0 +1,6 @@
+
+variable "incoming_traffic_policy" { default = null }
+variable "location" { default = null }
+variable "name" {}
+variable "resource_group_name" { default = null }
+variable "tags" { default = null }
