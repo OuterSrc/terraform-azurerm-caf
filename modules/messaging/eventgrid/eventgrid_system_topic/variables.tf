fb7e8e9284619f2968b22a93fdd8b7a5d7f23ca0
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2025-06-04 16:52:50 +0100

    Revert "Main"

--- modules/messaging/eventgrid/eventgrid_system_topic/variables.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic/variables.tf
@@ -1,8 +1,8 @@
 variable "global_settings" {
-  description = "Global settings object"
+  description = "Global settings object (see module README.md)"
 }
 variable "client_config" {
-  description = "Client configuration object."
+  description = "Client configuration object (see module README.md)."
 }
 variable "settings" {
   description = "(Required) Used to handle passthrough paramenters."
@@ -13,10 +13,5 @@ variable "remote_objects" {
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
-  default     = {}
-}
-variable "location" {
-  description = "Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
-  default     = null
+  type        = bool
 }
