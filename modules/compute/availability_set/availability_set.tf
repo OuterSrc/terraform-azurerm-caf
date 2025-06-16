bd4d23ebae4768f66d73847ef94c31a17fa55aac
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2025-06-16 14:37:06 +0100

    explicit tags

--- modules/compute/availability_set/availability_set.tf
+++ modules/compute/availability_set/availability_set.tf
@@ -2,9 +2,7 @@ resource "azurerm_availability_set" "avset" {
   name                         = azurecaf_name.avset.result
   location                     = var.location
   resource_group_name          = var.resource_group_name
-  # tags                         = local.tags
-  # tags                         = merge(local.tags, try(var.settings.tags, null))
-  # tags                         = try(var.settings.tags, null)
+  tags                         = merge(local.tags, try(var.tags, null))
   platform_update_domain_count = var.settings.platform_update_domain_count
   platform_fault_domain_count  = var.settings.platform_fault_domain_count
   managed                      = try(var.settings.managed, true)
