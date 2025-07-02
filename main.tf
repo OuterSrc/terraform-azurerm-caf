2a530fbcd1de2df169769755067bbdc6ab4f095a
ml-justinhewitt <72511644+ml-justinhewitt@users.noreply.github.com>
2025-07-02 11:57:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "=1.2.29"
+      version = "= 1.2.29"
     }
     null = {
       source = "hashicorp/null"
