55a75d2ecf32350b977144658e4fa20cbaa20f3c
ml-abijithnagesh <abijithn@microland.com>
2025-10-01 14:44:45 +0530

    revert changes

--- mssql_servers.tf
+++ mssql_servers.tf
@@ -49,10 +49,12 @@ resource "azurerm_mssql_server_extended_auditing_policy" "mssql" {
     if try(value.extended_auditing_policy, null) != null
   }
 
-  log_monitoring_enabled = try(each.value.extended_auditing_policy.log_monitoring_enabled, false)
-  server_id              = module.mssql_servers[each.key].id
-  storage_endpoint       = data.azurerm_storage_account.mssql_auditing[each.key].primary_blob_endpoint
-  retention_in_days      = try(each.value.extended_auditing_policy.retention_in_days, null)
+  log_monitoring_enabled                  = try(each.value.extended_auditing_policy.log_monitoring_enabled, false)
+  server_id                               = module.mssql_servers[each.key].id
+  storage_endpoint                        = data.azurerm_storage_account.mssql_auditing[each.key].primary_blob_endpoint
+  storage_account_access_key              = data.azurerm_storage_account.mssql_auditing[each.key].primary_access_key
+  storage_account_access_key_is_secondary = false
+  retention_in_days                       = try(each.value.extended_auditing_policy.retention_in_days, null)
 }
 
 module "mssql_failover_groups" {
